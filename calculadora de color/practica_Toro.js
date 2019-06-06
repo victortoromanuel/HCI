3f69a00ed445f219efa74b76ae2dbcc87c778333
victortoromanuel <victortoromanuel@gmail.com>
2019-06-06 07:01:19 -0500

    Add files via upload

---
+++ calculadora de color/practica_Toro.js
@@ -0,0 +1,65 @@
+
+function rgb(cad){
+	var ans = "";
+	var n = cad.split(",");
+	var num = n.map(n => Number(n));
+	for(var i in num){
+		let div = num[i]/16;
+		let ent = Math.trunc(div);
+		let res = num[i]%16;
+		if(ent < 10){
+			ans += ent;
+		}else{
+			ans += ent.toString(16).toUpperCase();
+		}
+
+		if(res < 10){
+			ans += res;
+		}else{
+			ans += res.toString(16).toUpperCase();
+		}
+	}
+	console.log(ans);
+	return ans;
+}
+
+function hexa(cad){
+	var ans = [];
+	for(var i = 1; i < 6; i+=2){
+		var tmp = [];
+		for(var j = i; j >= i-2; j--){
+			tmp.push(parseInt(cad[j],16));
+		}
+		ans.push(tmp[0] + tmp[1]*16);
+	}
+	console.log(ans);
+	return ans;
+}
+
+function main(){
+	var cad1 = document.getElementById("enterColorCodeRGB").value;
+	var cad2 = document.getElementById("enterColorCodeHexa").value;
+	var n1 = cad1.length;
+	var n2 = cad2.length;
+	if(n1 != 0){
+		var hex = rgb(cad1);
+		document.getElementById("enterColorCodeHexa").value = hex;
+	}else{
+		var hex = cad2;
+		var colo = hexa(cad2);
+		document.getElementById("enterColorCodeRGB").value = colo;
+	}
+	var hashtag = "#";
+	hashtag += hex;
+	document.getElementById("color").style.backgroundColor = hashtag;
+	document.getElementById("colour").style.backgroundColor = hashtag;
+}
+
+function color(){
+	var cad = document.getElementById("enterColorCodeHexa").value;
+	var hashtag = "#";
+	hashtag += cad;
+	document.getElementById("color").style.backgroundColor = hashtag;
+	document.getElementById("colour").style.backgroundColor = hashtag;
+}
+
